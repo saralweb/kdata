ae0c909d6a725feb6b3328cbbbf9a1f3b6940f0e
kavi <kwrites@saralweb.com>
2020-07-23 19:29:58 +0530

    adding icfr files

--- src/sql/icfr_dump.sql
+++ src/sql/icfr_dump.sql
@@ -1,30 +1,30 @@
 \pset footer off
 
 \C India
-\o ../generated/icfr/India_icfr.csv
+\o ../../generated/icfr/India_icfr.csv
 select * from cfr_analysis('India');
 
 \C Germany
-\o ../generated/icfr/Germany_icfr.csv
+\o ../../generated/icfr/Germany_icfr.csv
 select * from cfr_analysis('Germany');
 
 \C Sweden
-\o ../generated/icfr/Sweden_icfr.csv
+\o ../../generated/icfr/Sweden_icfr.csv
 select * from cfr_analysis('Sweden');
 
 \C France
-\o ../generated/icfr/France_icfr.csv
+\o ../../generated/icfr/France_icfr.csv
 select * from cfr_analysis('France');
 
 \C Italy
-\o ../generated/icfr/Italy_icfr.csv
+\o ../../generated/icfr/Italy_icfr.csv
 select * from cfr_analysis('Italy');
 
 \C Spain
-\o ../generated/icfr/Spain_icfr.csv
+\o ../../generated/icfr/Spain_icfr.csv
 select * from cfr_analysis('Spain');
 
 \C US
-\o ../generated/icfr/US_icfr.csv
+\o ../../generated/icfr/US_icfr.csv
 select * from cfr_analysis('US');
 
